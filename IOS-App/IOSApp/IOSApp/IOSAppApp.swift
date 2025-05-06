0bfb1758bf29645f6c18341dcce84ebac4b7323f
William Vance <william.vance@fhnw.edu>
2025-05-06 08:30:23 -0600

    Mnios (#3)
    * Setting up the IOS app
    
    * Fix the app
    
    ---------
    
    Co-authored-by: Molly <norman.j.molly@gmail.com>

---
+++ IOS-App/IOSApp/IOSApp/IOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppApp.swift
+//  IOSApp
+//
+//  Created by Molly Norman on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
